322f7d6b479b00e4001a5b9a87db48a79980132b
sridharsilver <sridharsilver@gmail.com>
2026-06-06 01:40:36 +0530

    Monkeypatch Node DOM operations to globally prevent React unmount crashes caused by browser extensions

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,27 @@ import App from "./App.tsx";
 import { ErrorBoundary } from "./components/ErrorBoundary";
 import "./index.css";
 
+// Patch DOM to prevent React crashes from browser extensions (e.g. Google Translate) modifying DOM nodes
+if (typeof Node === "function" && Node.prototype) {
+  const originalRemoveChild = Node.prototype.removeChild;
+  Node.prototype.removeChild = function (child: Node) {
+    if (child.parentNode !== this) {
+      console.warn("Prevented React crash: Cannot remove a child from a different parent", child, this);
+      return child;
+    }
+    return originalRemoveChild.apply(this, arguments as any);
+  };
+
+  const originalInsertBefore = Node.prototype.insertBefore;
+  Node.prototype.insertBefore = function (newNode: Node, referenceNode: Node | null) {
+    if (referenceNode && referenceNode.parentNode !== this) {
+      console.warn("Prevented React crash: Cannot insert before a reference node from a different parent", referenceNode, this);
+      return newNode;
+    }
+    return originalInsertBefore.apply(this, arguments as any);
+  };
+}
+
 createRoot(document.getElementById("root")!).render(
   <ErrorBoundary>
     <App />
